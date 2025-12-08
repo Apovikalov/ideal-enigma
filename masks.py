76494bb7a6a04f9714f0bd2920265d79cf536955
Андрей Повикалов <apovikalov@bk.ru>
2025-12-08 16:15:22 +0600

    Домашка 10.2

---
+++ masks.py
@@ -0,0 +1,22 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция принимает на вход номер карты и возвращает её маску"""
+    if len(card_number) == 16:
+        masked = card_number[0:4]
+        masked += " "
+        masked += card_number[4:6]
+        masked += "** **** "
+        masked += card_number[-4:]
+    elif len(card_number) == 13:
+        masked = card_number[0:6]
+        masked += "***"
+        masked += card_number[-4:-1]
+        masked += " "
+        masked += card_number[-1]
+    return masked
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция принимает на вход номер счёта и возвращает его маску"""
+    masked = "**"
+    masked += account_number[-4:]
+    return masked
---
+++ tests/test_widget.py
@@ -0,0 +1,21 @@
+import pytest
+
+from widget import get_date, mask_account_card
+
+
+@pytest.mark.parametrize('card_number, complete_number',
+                         [('Visa Platinum 7000792289606361', 'Visa Platinum 7000 79** **** 6361'),
+                          ('Maestro 1596837868705199', 'Maestro 1596 83** **** 5199'),
+                          ('Счет 73654108430135874305', 'Счет **4305'),
+                          ('Счет 12345678901234567890', 'Счет **7890')])
+def test_mask_account_card(card_number, complete_number):
+    result = mask_account_card(card_number)
+    assert (result == complete_number)
+
+
+@pytest.mark.parametrize('date_and_time, complete_date',
+                         [('2024-03-11T02:26:18.671407', '11.03.2024'),
+                          ('1999-07-31T02:26:18.671407', '31.07.1999')])
+def test_get_date(date_and_time, complete_date):
+    result = get_date(date_and_time)
+    assert (result == complete_date)
