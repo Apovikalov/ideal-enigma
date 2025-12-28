2960976628bc046cd218e5311f2149aadc4c691b
Андрей Повикалов <apovikalov@bk.ru>
2025-12-28 18:09:52 +0600

    Домашка 12.1

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,16 @@
 import logging
 
 masks_logger = logging.getLogger("masks")
+masks_logger.setLevel(logging.DEBUG)  # Уровень логирования не ниже DEBUG
+
+masks_file_handler = logging.FileHandler("logs/masks.log", mode="w")
+masks_file_handler.setLevel(logging.DEBUG)
+
+masks_file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+masks_file_handler.setFormatter(masks_file_formatter)
+
+masks_logger.addHandler(masks_file_handler)
+
 
 def get_mask_card_number(card_number: int) -> str:
     """Функция принимает на вход номер карты и возвращает её маску"""
@@ -9,6 +19,7 @@ def get_mask_card_number(card_number: int) -> str:
     masked += str(card_number)[4:6]
     masked += "** **** "
     masked += str(card_number)[-4:]
+    masks_logger.info(f"Получена маска номера карты: {masked}")
     return masked
 
 
@@ -16,4 +27,5 @@ def get_mask_account(account_number: int) -> str:
     """Функция принимает на вход номер счёта и возвращает его маску"""
     masked = "**"
     masked += str(account_number)[-4:]
+    masks_logger.info(f"Получена маска номера счёта: {masked}")
     return masked
---
+++ tests/test_masks.py
@@ -0,0 +1,21 @@
+import pytest
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+@pytest.mark.parametrize('card_number, masked',
+                         [('7000792289606361', '7000 79** **** 6361'),
+                          ('1234567890123456', '1234 56** **** 3456')])
+def test_get_mask_card_number(card_number, masked):
+    result = get_mask_card_number(card_number)
+    assert (result == masked)
+
+
+@pytest.mark.parametrize('account_number, masked',
+                         [('73654108430135874305', '**4305'),
+                          ('12345678901234567890', '**7890'),
+                          ('1234567890123456', '**3456'),
+                          ('1234567890123', '**0123')])
+def test_get_mask_account(account_number, masked):
+    result = get_mask_account(account_number)
+    assert (result == masked)
