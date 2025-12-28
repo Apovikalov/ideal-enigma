3b8c4e618df40cf74571f6183b3475300b9c4b54
Андрей Повикалов <apovikalov@bk.ru>
2025-12-28 18:30:27 +0600

    Домашка 12.2

--- src/masks.py
+++ src/masks.py
@@ -14,18 +14,26 @@ masks_logger.addHandler(masks_file_handler)
 
 def get_mask_card_number(card_number: int) -> str:
     """Функция принимает на вход номер карты и возвращает её маску"""
-    masked = str(card_number)[0:4]
-    masked += " "
-    masked += str(card_number)[4:6]
-    masked += "** **** "
-    masked += str(card_number)[-4:]
-    masks_logger.info(f"Получена маска номера карты: {masked}")
-    return masked
+    if len(str(card_number)) != 16:
+        masks_logger.error("Некорректный номер карты")
+        return ""
+    else:
+        masked = str(card_number)[0:4]
+        masked += " "
+        masked += str(card_number)[4:6]
+        masked += "** **** "
+        masked += str(card_number)[-4:]
+        masks_logger.info(f"Получена маска номера карты: {masked}")
+        return masked
 
 
 def get_mask_account(account_number: int) -> str:
     """Функция принимает на вход номер счёта и возвращает его маску"""
-    masked = "**"
-    masked += str(account_number)[-4:]
-    masks_logger.info(f"Получена маска номера счёта: {masked}")
-    return masked
+    if len(str(account_number)) != 20:
+        masks_logger.error("Некорректный номер счёта")
+        return ""
+    else:
+        masked = "**"
+        masked += str(account_number)[-4:]
+        masks_logger.info(f"Получена маска номера счёта: {masked}")
+        return masked
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -14,8 +14,8 @@ def test_get_mask_card_number(card_number, masked):
 @pytest.mark.parametrize('account_number, masked',
                          [('73654108430135874305', '**4305'),
                           ('12345678901234567890', '**7890'),
-                          ('1234567890123456', '**3456'),
-                          ('1234567890123', '**0123')])
+                          ('78901234567890123456', '**3456'),
+                          ('45678901234567890123', '**0123')])
 def test_get_mask_account(account_number, masked):
     result = get_mask_account(account_number)
     assert (result == masked)
