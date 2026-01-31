75999f8dde24905fbec223bbbf1b3246f5a1a99b
Андрей Повикалов <apovikalov@bk.ru>
2026-01-31 16:11:38 +0600

    Домашка 14.2 (1)

--- src/classes.py
+++ src/classes.py
@@ -16,7 +16,7 @@ class Category:
     """Класс для представления категории"""
     name: str
     description: str
-    products: list
+    __products: list
     category_count = 0
     product_count = 0
 
@@ -26,3 +26,6 @@ class Category:
         self.products = products
         Category.category_count += 1
         Category.product_count += len(self.products)
+
+    def add_product(self, product):
+        self.products.append(product)
