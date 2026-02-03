437c4bf3921ca3f75644888ae7beae4bf7a073d2
Андрей Повикалов <apovikalov@bk.ru>
2026-02-03 12:00:38 +0600

    Домашка 15.1

--- src/classes.py
+++ src/classes.py
@@ -30,7 +30,6 @@ class Product:
         else:
             self.__price = val
 
-
     def __str__(self):
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
@@ -39,16 +38,27 @@ class Category:
     """Класс для представления категории"""
     name: str
     description: str
-    products: list
+    __products: list
     category_count = 0
     product_count = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
-        Category.product_count += len(self.products)
+        Category.product_count += len(self.__products)
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        list_of_prod = []
+        for i in self.__products:
+            list_of_prod.append(f"{i.name}, {i.price} руб. Остаток: {i.quantity} шт.")
+        return list_of_prod
 
     def __str__(self):
         return f"{self.name}, количество продуктов: {self.product_count} шт."
