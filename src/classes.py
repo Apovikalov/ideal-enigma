1050377c80ac50e27de394b432b72afc21546679
Андрей Повикалов <apovikalov@bk.ru>
2026-02-03 11:58:58 +0600

    Домашка 15.1

--- src/classes.py
+++ src/classes.py
@@ -2,14 +2,34 @@ class Product:
     """Класс для представления продукта"""
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, prod):
+        name = prod["name"]
+        description = prod["description"]
+        price = prod["price"]
+        quantity = prod["quantity"]
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, val):
+        if val <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = val
+
 
     def __str__(self):
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
